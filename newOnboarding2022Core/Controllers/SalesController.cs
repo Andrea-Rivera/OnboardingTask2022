b5857f789dd36d390ae52b88800add93125b3329
Andrea-Rivera <36813115+Andrea-Rivera@users.noreply.github.com>
2022-07-21 15:58:22 +1000

    Updating Sales Controller

--- newOnboarding2022Core/Controllers/SalesController.cs
+++ newOnboarding2022Core/Controllers/SalesController.cs
@@ -11,5 +11,122 @@ namespace newOnboarding2022Core.Controllers
     [ApiController]
     public class SalesController : ControllerBase
     {
+    private OnboardingDBContext _dBContext;
+        public SalesController(OnboardingDBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+
+        //GET ROUTE/api/sales/getsales
+        [HttpGet("GetSales")]
+        public IActionResult Get()
+        {
+            try
+            {
+                var sale = _dBContext.Sales
+                    .Include(x => x.Store)
+                    .Include(x => x.Customer)
+                    .Include(x => x.Product);
+
+
+                return Ok(sale.ToList());
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(400, "An error has ocurred");
+            }
+
+        }
+
+        //POST ROUTE/api/sales/createsales
+        [HttpPost("CreateSales")]
+        public IActionResult Create([Bind("StoreId, CustomerId, ProductId, DateSold")][FromBody] Sales createdSale)
+        {
+            
+            try
+            {    
+                _dBContext.Sales.Add(createdSale);
+              
+                _dBContext.SaveChanges();
+
+                // Loading navigation properties.
+                _dBContext.Entry(createdSale).Reference(x => x.Store).Load();
+                _dBContext.Entry(createdSale).Reference(x => x.Customer).Load();
+                _dBContext.Entry(createdSale).Reference(x => x.Product).Load();
+                
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(400, "An error has ocurred");
+            }
+            //Get all sales in a List
+            var saleDisplay = _dBContext.Sales.ToList();
+            return Ok(saleDisplay);
+        }
+
+        //PUT ROUTE/api/sales/Updatesales
+        [HttpPut("UpdateSales")]
+        public IActionResult Update([Bind("Id, StoreId, CustomerId, ProductId, DateSold")][FromBody] Sales updateSale)
+        {
+
+            try
+            {
+                Sales sale = _dBContext.Sales.Single(x => x.Id == updateSale.Id);
+
+                sale.StoreId = updateSale.StoreId;
+                sale.CustomerId = updateSale.CustomerId;
+                sale.ProductId = updateSale.ProductId;
+                sale.DateSold = updateSale.DateSold;
+                //Save changes in the database.
+                _dBContext.SaveChanges();
+
+                // Loading navigation properties.
+                _dBContext.Entry(updateSale).Reference(x => x.Store).Load();
+                _dBContext.Entry(updateSale).Reference(x => x.Customer).Load();
+                _dBContext.Entry(updateSale).Reference(x => x.Product).Load();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(400, "An error has ocurred");
+            }
+            //Get all sales in a List
+            var saleDisplay = _dBContext.Sales.ToList();
+            return Ok(saleDisplay);
+        }
+        //  DELETE ROUTE/api/sales/deletesales/Id
+        [HttpDelete("DeleteSales/{Id}")]
+        public IActionResult Delete([FromRoute]int Id)
+        {
+            try
+            {
+                var sale = _dBContext.Sales.FirstOrDefault(x => x.Id == Id);
+                if (sale == null)
+                {
+                    return StatusCode(404, "An error has ocurred");
+                }
+
+
+                _dBContext.Entry(sale).State = EntityState.Deleted;
+                _dBContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(400, "An error has ocurred"); ;
+            }
+
+            //Get all users
+            var saleDisplay = _dBContext.Customer.ToList();
+            return Ok(saleDisplay);
+        }
+
     }
 }
